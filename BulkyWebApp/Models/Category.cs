033920bc2d542fb1f808f7455e20c9f0c8850dbd
SaiyedRushan <rushan52@gmail.com>
2023-06-13 17:19:34 -0400

    Added category controller and views

--- BulkyWebApp/Models/Category.cs
+++ BulkyWebApp/Models/Category.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace BulkyWebApp.Models
 {
@@ -9,6 +10,8 @@ namespace BulkyWebApp.Models
         public int Id { get; set; }
         [Required]
         public string Name { get; set; }
+        [DisplayName("Display Order")]
+        [Range(1,100)]
         public int DisplayOrder { get; set; }
         public DateTime CreatedDateTime { get; set; } = DateTime.Now;
 
